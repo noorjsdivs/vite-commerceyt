14686cf0ce08fe2ddb80710310e3a754d7f87f8d
Noor Mohammad <noor.jsdivs@gmail.com>
2024-05-06 23:46:23 +0600

    Update Products.jsx

--- client/src/ui/Products.jsx
+++ client/src/ui/Products.jsx
@@ -9,7 +9,7 @@ const Products = ({ products }) => {
           shopping everyday
         </h1>
         <span className="w-20 h-[3px] bg-black"></span>
-        <p className="max-w-[700px] text-gray-600 text-center">
+        <p className="max-w-[700px] text-gray-600 text-cente px-4">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo,
           quos fugit inventore, cumque quae corporis ratione tenetur eos
           voluptates neque magnam soluta aperiam omnis perspiciatis reiciendis
@@ -17,7 +17,7 @@ const Products = ({ products }) => {
         </p>
       </div>
       {/* =================== Products Start here ================= */}
-      <div className="max-w-screen-xl mx-auto grid grid-cols-4 gap-10 py-10">
+      <div className="max-w-screen-xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 py-10 px-4">
         {products.map((item) => (
           <ProductsCard key={item._id} product={item} />
         ))}
